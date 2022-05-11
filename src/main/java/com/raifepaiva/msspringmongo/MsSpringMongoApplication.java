df0e3a358172df9fbcf41b214da4e280f134432a
RaifePaiva <raife.tec@gmail.com>
2022-05-11 15:21:00 -0300

    Projeto base

---
+++ src/main/java/com/raifepaiva/msspringmongo/MsSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.raifepaiva.msspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringMongoApplication.class, args);
+	}
+
+}
